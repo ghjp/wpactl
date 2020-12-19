ba305f2247174b81ea01c7b69ec118270a1acde7
Dr. Johann Pfefferl <pfefferl@gmx.net>
2020-12-19 21:25:26 +0100

    Renamed const variables
    Signed-off-by: Dr. Johann Pfefferl <pfefferl@gmx.net>

--- src/jp.net/wlctl/main.go
+++ src/jp.net/wlctl/main.go
@@ -14,18 +14,16 @@ var (
 )
 
 const (
-	iface        = "wlxf0b0148be468"
-	dbus_service = "fi.w1.wpa_supplicant1"
-	dbus_path    = "/fi/w1/wpa_supplicant1"
-	dbus_iface   = dbus_service
+	DbusService = "fi.w1.wpa_supplicant1"
+	DbusPath    = "/fi/w1/wpa_supplicant1"
+	DbusIface   = DbusService
 )
 
-func get_iface_path(bo dbus.BusObject) dbus.ObjectPath {
-	var obj_iface_path dbus.ObjectPath
-	if err := bo.Call(dbus_iface+".GetInterface", 0, *optIface).Store(&obj_iface_path); err != nil {
+func get_iface_path(bo dbus.BusObject) (obj_iface_path dbus.ObjectPath) {
+	if err := bo.Call(DbusIface+".GetInterface", 0, *optIface).Store(&obj_iface_path); err != nil {
 		log.Fatal(err)
 	}
-	return obj_iface_path
+	return
 }
 
 func main() {
@@ -37,30 +35,30 @@ func main() {
 	}
 	defer conn.Close()
 
-	obj := conn.Object(dbus_service, dbus_path)
+	obj := conn.Object(DbusService, DbusPath)
 	switch *optMode {
 	case "scan":
 		obj_iface_path := get_iface_path(obj)
-		iface_obj := conn.Object(dbus_service, obj_iface_path)
+		iface_obj := conn.Object(DbusService, obj_iface_path)
 		scan_args := make(map[string]interface{})
 		scan_args["Type"] = "active"
 		//scan_args["AllowRoam"] = true
 
 		log.Println("Trigger scan on interface", *optIface)
-		if err = iface_obj.Call(dbus_iface+".Interface.Scan", 0, scan_args).Err; err != nil {
+		if err = iface_obj.Call(DbusIface+".Interface.Scan", 0, scan_args).Err; err != nil {
 			log.Fatal(err)
 		}
 	case "up":
 		ci_args := make(map[string]interface{})
 		ci_args["Ifname"] = *optIface
 		ci_args["ConfigFile"] = *optCfgFile
-		if err = obj.Call(dbus_iface+".CreateInterface", 0, ci_args).Err; err != nil {
+		if err = obj.Call(DbusIface+".CreateInterface", 0, ci_args).Err; err != nil {
 			log.Fatal(err)
 		}
 		log.Println("Interface", *optIface, "now managed")
 	case "down":
 		oip := get_iface_path(obj)
-		if err = obj.Call(dbus_iface+".RemoveInterface", 0, oip).Err; err != nil {
+		if err = obj.Call(DbusIface+".RemoveInterface", 0, oip).Err; err != nil {
 			log.Fatal(err)
 		}
 		log.Println("Interface", *optIface, "no longer managed")
